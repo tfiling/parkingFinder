4504c078d3930d928e721be1f0421a0a88873f8c
tfiling <galtfilin@gmail.com>
2017-09-07 18:51:25 +0300

    solved the deprecation warning when connecting to DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+	keepAlive: true,
+	reconnectTries: Number.MAX_VALUE,
+	useMongoClient: true
+});
 
 module.exports = {mongoose};
